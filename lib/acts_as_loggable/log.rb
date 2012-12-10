d1614a027a6b6b138d50de6951c72644990135aa
Jason Denney <jdenney@lgscout.com>
2012-12-10 00:51:34 -0500

    Get a Log's Action based of of loggable_type…. is there a better way?

--- lib/acts_as_loggable/log.rb
+++ lib/acts_as_loggable/log.rb
@@ -14,5 +14,9 @@ module ActsAsLoggable
     validates_presence_of :start_date
     validates_presence_of :end_date
     validates_length_of :description, :maximum => 255
+
+    def action
+      "ActsAsLoggable::#{self.loggable_type}Action".constantize.find_by_id(self.action_id)
+    end
   end
 end
