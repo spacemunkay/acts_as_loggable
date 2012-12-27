5eb7d17122e48c83dd962053a6d0d7c005a983fe
Jason Denney <jdenney@lgscout.com>
2012-12-26 21:23:58 -0500

    Make log action polymorphic, add sortbyduration scope

--- lib/acts_as_loggable/log.rb
+++ lib/acts_as_loggable/log.rb
@@ -1,9 +1,14 @@
 module ActsAsLoggable
   class Log < ::ActiveRecord::Base
     belongs_to :loggable, :polymorphic => true
+    belongs_to :log_action, :polymorphic => true
+
+    #quickfix for Netzke
+    belongs_to :user_action, :foreign_key => 'log_action_id'
+    belongs_to :bike_action, :foreign_key => 'log_action_id'
 
     attr_accessible :loggable_type, :loggable_id, :logger_type, :logger_id, :context,
-                    :start_date, :end_date, :description, :action_id
+                    :start_date, :end_date, :description, :log_action_id, :log_action_type
 
     ### ASSOCIATIONS:
 
@@ -11,13 +16,18 @@ module ActsAsLoggable
 
     ### VALIDATIONS:
 
-    validates_presence_of :action_id
+    validates_presence_of :log_action_id
     validates_presence_of :start_date
     validates_presence_of :end_date
     validates_length_of :description, :maximum => 255
+    
+    scope :sort_by_duration, lambda { |direction| order("(julianday(end_date) - julianday(start_date)) #{direction}") }
+    
+
 
     def action
-      "ActsAsLoggable::#{self.loggable_type}Action".constantize.find_by_id(self.action_id)
+      #"ActsAsLoggable::#{self.loggable_type}Action".constantize.find_by_id(self.action_id)
+      self.log_action
     end
 
     def loggable_path
